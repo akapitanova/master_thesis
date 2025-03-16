96b99bfc4e28f8d80b928dbabf87fa355f4bd643
Anna Kapitanova <kapitanovaanna333@gmail.com>
2025-03-16 13:46:09 +0100

    preprocessing of stg 7

--- src/dataset.py
+++ src/dataset.py
@@ -5,8 +5,8 @@ from torch.utils.data import DataLoader, Dataset
 
 class CustomDataset(Dataset):
     def __init__(self, x_data, y_data):
-        self.x_data = x_data  # 1D feature vectors
-        self.y_data = y_data  # Target labels
+        self.x_data = x_data  
+        self.y_data = y_data  
 
     def __len__(self):
         return len(self.x_data)
